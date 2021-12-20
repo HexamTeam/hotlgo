69c7e7fd5f1f9ae4eb820f84da8fd62a984d6b91
Waruni Imalsha <WaruniImalsha@horizoncampus.edu.lk>
2021-12-21 02:08:05 +0530

    create headericon.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from './component/home/Home';
 function App() {
   return (
     <div className="App">
-      <Headericon/>
+      {/* <Headericon/> */}
       <Home/>
 
     </div>
--- src/component/header/Headericon.js
+++ src/component/header/Headericon.js
@@ -1,17 +1,80 @@
 import logo from '../../resources/icon/LogoImg.jpg';
-import { Button } from '@material-ui/core';
 import '../../css/rootcss/headericon-btn.css';
-
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import React from "react";
+import { Button } from '@mui/material';
+import IconButton from '@material-ui/core/IconButton';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 
 
 const Headericon = () => {
+    
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+        const handleClick = (event) => {
+            setAnchorEl(event.currentTarget);
+        };
+        const handleClose = () => {
+            setAnchorEl(null);
+        };
+
+
+
+
     //this is the main component in header
     return(
         <div className='headericon-whitespace'>
+        <button className='header-button-logobtn' ><img src={logo} className='header-button-logobtn-image'/></button>
         
-        <Button className='header-button-logobtn' ><img src={logo} alter='' className='header-button-logobtn-image'/></Button>
-       
+         <div className='icon-button-pading'>
+         <Button
+               
+               id="demo-positioned-button"
+               aria-controls="demo-positioned-menu"
+               aria-haspopup="true"
+               aria-expanded={open ? 'true' : undefined}
+               onClick={handleClick}
+               class="header-icon-button btn-secondary pull-right btn-sm"
+               className='header-icon-button'
+           
+            >
+               
+               <IconButton color="inherit"  >
+                   <MenuIcon fontSize="small"/>
+                   <PersonOutlineIcon fontSize="small" /> 
+                   </IconButton>
+           </Button>
+            
+           
+           <Menu
+               id="demo-positioned-menu"
+               aria-labelledby="demo-positioned-button"
+               anchorEl={anchorEl}
+               open={open}
+               onClose={handleClose}
+               anchorOrigin={{
+               vertical: 'top',
+               horizontal: 'left',
+               }}
+               transformOrigin={{
+               vertical: 'top',
+               horizontal: 'left',
+               }}
+           >
+               <MenuItem onClick={handleClose}>Log In</MenuItem><br/>
+               <MenuItem onClick={handleClose}>Register</MenuItem>
+               
+           </Menu>
+         </div>
+
+               
+        <div className='hotelier-pading'>
+        <button className='header-button-hotelier'>Become a hotelier</button>
         </div>
+
+         </div>
     )
 }
 
