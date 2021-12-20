dabb42dda41e722db84825ebc50c2c5d381f844b
Lakshan Madhubashika <LakshanMadhubashika@horizoncampus.edu.lk>
2021-12-21 03:04:35 +0530

    chnge search bar

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -1,13 +1,20 @@
 import '../../css/rootcss/home.css';
 import SearchIcon from '@mui/icons-material/Search';
 import Headericon from '../header/Headericon';
+// import TextField from '@mui/material/TextField';
+// import DateRangePicker from '@mui/lab/DateRangePicker';
+// import AdapterDateFns from '@mui/lab/AdapterDateFns';
+// import LocalizationProvider from '@mui/lab/LocalizationProvider';
+// import Box from '@mui/material/Box';
+
 
 
 
 const Home = () => {
 
     return(
-        <>            
+        <>           
+        
             <div className="home-headerimg">
                 <Headericon/>
                 <center>
@@ -15,11 +22,45 @@ const Home = () => {
                         <div className='home-searchbar-row'>
                             <div><SearchIcon className='home-searchbar-icon1'/></div>
                             <div><input type='text' className='home-searchbar-input-text' placeholder="Hi, Where do want to go... "/></div>
-                        </div>
-                        
-                        
+                        </div>                       
                     </div>  
-                    <div className='home-searchbar-max'></div>                  
+                    <div className='home-searchbar-max'>
+                        <div className='home-searchbar-max-row'>
+                            <div className='home-searchbar-max-firstdiv'>
+                                <table>
+                                    <tbody>
+                                        <tr><div className='home-searchbar-location-text'>Location</div></tr>
+                                        <tr><input type='text' className='home-searchbar-input-text1' placeholder="Hi, Where do want to go... "/></tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div>
+                                <table>
+                                    <tbody>
+                                        <tr><div className='home-searchbar-location-text'>Check In</div></tr>
+                                        <tr><input type='text' className='home-searchbar-input-text1' placeholder="Hi, Where do want to go... "/></tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div>
+                                <table>
+                                    <tbody>
+                                        <tr><div className='home-searchbar-location-text'>Check Out</div></tr>
+                                        <tr><input type='text' className='home-searchbar-input-text1' placeholder="Hi, Where do want to go... "/></tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div>
+                                <table>
+                                    <tbody>
+                                        <tr><div className='home-searchbar-location-text'>Guest Room</div></tr>
+                                        <tr><div>2 adults . 1 room</div></tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+
+                    </div>                  
                 </center>                 
             </div> 
                       
