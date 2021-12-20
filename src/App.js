d63de53e5ed8903adfac5094e8eff203f103fe44
Waruni Imalsha <WaruniImalsha@horizoncampus.edu.lk>
2021-12-20 14:13:12 +0530

    add button and icon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Headericon from './component/header/Headericon';
 import Button from './component/shere/Button';
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Button/>
+      <Headericon/>
     </div>
   );
 }
--- src/component/header/Headericon.js
+++ src/component/header/Headericon.js
@@ -1,5 +1,22 @@
+//import '../../resources/icon/Logoiimg.jpg'
+import { Button } from '@material-ui/core';
+import '../../css/rootcss/headericon-btn.css';
+import IconButton from '@material-ui/core/IconButton';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+
+
 const Headericon = () => {
     //this is the main component in header
+    return(
+        <div>
+        <button className='header-button-btn'>click</button>
+        <Button>hj</Button>
+        <IconButton color="primary">
+  <WhatshotIcon fontSize="large" />
+</IconButton>
+
+        </div>
+    )
 }
 
 export default Headericon
