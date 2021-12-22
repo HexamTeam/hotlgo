5f0766db84779daf5bec6bcebebe42f49eef2b42
Lakshan Madhubashika <LakshanMadhubashika@horizoncampus.edu.lk>
2021-12-22 22:00:02 +0530

    add destination

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import './App.css';
 import Footer from './component/footer/Footer';
 import Headericon from './component/header/Headericon';
+import Chg from './component/home/Chg';
 import Destination from './component/home/Destination';
 import Home from './component/home/Home';
 import TopDeal from './component/home/TopDeal';
 
-
-
-
 function App() {
-
-  
+ 
   return (
     <div className="App">
+      {/* <TestSider/> */}
       <Headericon/>
       <Home/> 
-      {/* <Destination/> */}
+      <Destination/>
+      <Chg/>
+      
       <TopDeal />
+
 
       <Footer/>
     </div>
